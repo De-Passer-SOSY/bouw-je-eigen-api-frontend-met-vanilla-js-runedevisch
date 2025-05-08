bcecc2bff26f5e04d1579d198bba372c9f3af6ba
runedevisch <rune.devisch@leerling.de-passer.be>
2025-05-08 11:47:33 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/footballData", async (req, res) => {
+    try{
+      const footballData = await db("footballData");
+      res.status(200).json(footballData)
+    } catch{
+        res.status(500).json({message:"Internal server error"})
+    }
+})
+
 app.listen(3333, () => {
     console.log("Server is running on port 3333");
 })
