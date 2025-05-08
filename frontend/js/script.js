b8ff7e1c1b5fe06ff488d505a5f90e0e5cb31026
runedevisch <rune.devisch@leerling.de-passer.be>
2025-05-08 11:58:06 +0200

    Update script.js

---
+++ frontend/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector(). addEventListener("click", match);
+    console.log("De pagina werd ingeladen.");
+    match();
+}
+
+function match() {
+    console.log("match");
+}
